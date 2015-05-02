71549e5a0ba30eea116a3230e2f49d838bc15998
Drew Maughan <codeur@perfectzerolabs.com>
2015-05-02 16:44:53 +0100

    Ability to display information about Flickr photos.

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ The display language for the feed. See the feeds page for feed language informat
 
 
 // Angular app.
-var app         = angular.module('fleek', ['ui.router', 'ngResource']);
+var app         = angular.module('fleek', ['ui.router', 'ngResource', 'ngSanitize']);
 var flickr_feed = 'https://api.flickr.com/services/feeds/photos_public.gne';
 
 const VIEW_HOME = 0;
@@ -29,10 +29,38 @@ const VIEW_HOME = 0;
 // inspired by http://jsfiddle.net/kstep/j25aj/9/
 // We have to use $resource instead of $http.get because of CORS (though this is one way of addressing the issue).
 app.factory('flickrPublicPhotos', function ($resource) {
-    return $resource('http://api.flickr.com/services/feeds/photos_public.gne',
-    	{ format: 'json', jsoncallback: 'JSON_CALLBACK' },
-    	{ 'load': { 'method': 'JSONP' }
-    });
+
+	var api = {
+		key   : 'ca2de41826a40f1100393ea5cfcc0e7b',
+		secret: 'dfc87f940d04691f'
+	};
+
+    return {
+    	feed: function() {
+			return $resource(
+				'http://api.flickr.com/services/feeds/photos_public.gne',
+				{ format: 'json', jsoncallback: 'JSON_CALLBACK' },
+				{ 'load': { 'method': 'JSONP' }
+			}).load();
+    	},
+
+    	// Fetch photo information.
+    	info: function(photo_id) {
+			return $resource(
+				'https://api.flickr.com/services/rest',
+				{
+					format      : 'json',
+					jsoncallback: 'JSON_CALLBACK',
+					method      : 'flickr.photos.getInfo',
+					api_key     : api.key,
+					photo_id    : photo_id
+				},
+				{
+					load: { method: 'JSONP' }
+				}
+			).load();
+		}
+    };
 });
 
 // UI ROUTER configuration.
@@ -48,25 +76,77 @@ app.config(function($stateProvider, $urlRouterProvider) {
             templateUrl: 'templates/home.tpl',
 			controller: function($scope, flickrPublicPhotos) {
 				$scope.model.view = VIEW_HOME;
-				$scope.model.data = flickrPublicPhotos.load();
+				$scope.model.data = flickrPublicPhotos.feed();
 			}
 		});
 });
 
 
+// Our SPA (single page application) Angular controller.
 app.controller('fleekCtrl', [
-	'$scope', '$http', '$state',
-	function ($scope, $http, $state) {
+	'$scope', '$http', '$state', 'flickrPublicPhotos',
+	function ($scope, $http, $state, flickrPublicPhotos) {
+
 
 	$scope.model = {
 		data: {},
-		view: VIEW_HOME
+		view: VIEW_HOME,
 	};
 
 
+	// Extract the link to the respective user's photostream from the URL of a
+	// photo.
 	$scope.getPhotosLink = function(photo_url) {
 		var exp     = new RegExp('(.*)\/[0-9]+');
 		var matches = photo_url.match( exp );
 		return matches[1];
-	}
+	};
+
+
+	// Extract the photo ID from the URL of a photo.
+	$scope.getPhotoID = function(photo_url) {
+		var exp     = new RegExp('\/([0-9]+)\/$');
+		var matches = photo_url.match( exp );
+		return matches[1];
+	};
+
+
+	$scope.getPhotoInfo = function(photo_url) {
+		var photo_id = $scope.getPhotoID(photo_url);
+
+		$data = flickrPublicPhotos.info(photo_id);
+		return $data.$promise;
+	};
+
+
+
+	$scope.getTitle = function(text) {
+		return text && text.length ? text : '<i>no title</i>';
+	};
+
+
+	$scope.getUserURL = function(user_id) {
+		return 'https://www.flickr.com/people/' + user_id + '/';
+	};
+
+
+	$scope.getPhotoURL = function(user_id, photo_id) {
+		return 'https://www.flickr.com/photos/' + user_id + '/' + photo_id;
+	};
+
+
+	$scope.showInfo = function(index) {
+
+		var item = $scope.model.data.items[ index ];
+
+		// Open a modal window.
+		$scope.model.post = 'loading';
+		$('#fleek-info').foundation('reveal', 'open');
+
+		$scope.getPhotoInfo(item.link).then(
+			function(response) {
+				$scope.model.post = response.photo;
+			}
+		);
+	};
 }]);
--- templates/home.tpl
+++ templates/home.tpl
@@ -9,7 +9,6 @@
 	<div class="results">
 
 		<div class="row" ng-repeat="row in model.data.items track by $index">
-
 			<!-- Photo on the left... -->
 			<div class="column small-4 medium-3">
 				<img ng-src="{{ row.media.m }}" alt="{{ row.title }} by {{ row.author }}">
@@ -18,20 +17,22 @@
 			<!-- Details on the right. -->
 			<div class="column small-8 medium-9">
 
-				<!-- Title of the post. -->
-				<h2>{{ row.title }}</h2>
+				<!-- Title of the post (links to more info). -->
+				<h2>
+					<a ng-click="showInfo( $index )" ng-bind-html="getTitle(row.title)"></a>
+				</h2>
 
 				<div class="row">
 
 					<!-- Published date. -->
 					<!-- We can use Foundation's pull-* and push* classes to position the date based on the screen size. -->
 					<div class="column small-4 small-push-3 medium-12 medium-push-0">
-						Published:
+						Published: {{ row.published }}
 					</div>
 
 					<!-- Post's author (as a link to their profile). -->
 					<div class="column small-3 small-pull-4 medium-pull-0">
-						<a href="{{ getPhotosLink( row.link ) }}" target="_blank">Post author</a>
+						<a ng-href="{{ getUserURL( row.author_id ) }}" target="_blank">Post author</a>
 					</div>
 
 					<!-- Link to the post. -->
@@ -43,6 +44,49 @@
 
 		</div>
 
+	</div>
+
+
+	<!-- Modal window to display details of a single post. -->
+	<div id="fleek-info" class="reveal-modal" data-reveal aria-hidden="true" role="dialog">
+
+		<div class="loading" ng-show="model.post === 'loading'">loading...</div>
+
+		<div ng-show="model.post !== 'loading'">
+			<!-- Heading. -->
+			<header class="row">
+				<div class="column medium-9">
+					<h2 ng-bind-html="getTitle( model.post.title._content )"></h2>
+
+					<!-- Post author. -->
+					<span class="author">
+						<a target="_blank">{{ model.post.owner.username }}</a>
+						<!-- <a ng-href="{{ getPhotosLink( row.link ) }}" target="_blank">Post author</a> -->
+					</span>
+					|
+					<span class="published">
+						Published: {{ model.post.dates.taken }}
+					</span>
+				</div>
+			</header>
+
+			<div class="row">
+				<div class="column medium-2">
+					<!-- image -->
+				</div>
+				<div class="column medium-10">
+					<p>{{ model.post.description._content || "no description" }}</p>
+					<ul class="tags">
+						<li ng-repeat="tag in model.post.tags">
+							{{ tag._content }}
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+		<!-- Back (close) button. -->
+		<a class="close-reveal-modal" aria-label="Close">Back</a>
 	</div>
 
 </div>
